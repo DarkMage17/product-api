139c93b465b30f0dae986d510dbb9ec8287b48e3
Josue Cuentas Jave <josue.c1012@gmail.com>
2022-01-10 20:18:13 -0500

    Created CRUD methods

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -1,4 +1,57 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, HttpStatus, NotFoundException, Param, Post, Put, Res } from '@nestjs/common';
+import { CreateProductDTO } from './dto/product.dto';
+import { ProductService } from './product.service';
 
 @Controller('product')
-export class ProductController {}
+export class ProductController {
+
+    constructor(private productService: ProductService) {}
+
+    @Post()
+    async createPost(@Res() res, @Body() createProductDTO: CreateProductDTO): Promise<CreateProductDTO>{
+        const product = await this.productService.createProduct(createProductDTO);
+        return res.status(HttpStatus.OK).json({
+            message: 'Product created successfully',
+            product
+        });
+    }
+
+    @Get()
+    async getProducts(@Res() res): Promise<CreateProductDTO[]>{
+        const products = await this.productService.getProducts();
+        return res.status(HttpStatus.OK).json({
+            message: 'Products',
+            products
+        });
+    }
+
+    @Get('/:productId')
+    async getProductById(@Res() res, @Param('productId') productId: string): Promise<CreateProductDTO>{
+        const product = await this.productService.getProductById(productId);
+        if(!product) throw new NotFoundException('Product does not exist');
+        return res.status(HttpStatus.OK).json({
+            message: 'Product obtained',
+            product
+        });
+    }
+
+    @Delete('/:productId')
+    async deleteProductById(@Res() res, @Param('productId') productId: string): Promise<CreateProductDTO>{
+        const deletedProduct = await this.productService.deleteProductById(productId);
+        if(!deletedProduct) throw new NotFoundException('Product does not exist');
+        return res.status(HttpStatus.OK).json({
+            message: 'Product deleted',
+            deletedProduct
+        });
+    }
+
+    @Put('/:productId')
+    async updateProduct(@Res() res, @Param('productId') productId, @Body() createProductDTO:CreateProductDTO){
+        const updatedProduct = await this.productService.updateProduct(productId, createProductDTO);
+        if(!updatedProduct) throw new NotFoundException('Product does not exist');
+        return res.status(HttpStatus.OK).json({
+            message: 'Product updated successfully',
+            updatedProduct
+        });
+    }
+}
--- src/product/product.service.ts
+++ src/product/product.service.ts
@@ -1,4 +1,35 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { CreateProductDTO } from './dto/product.dto';
+import { Product } from './interfaces/product.interface';
 
 @Injectable()
-export class ProductService {}
+export class ProductService {
+    constructor(@InjectModel('Product') private readonly productModel: Model<Product>) {}
+
+    async getProducts(): Promise<Product[]>{
+        const products = await this.productModel.find()
+        return products;
+    }
+
+    async getProductById(productId: string): Promise<Product>{
+        const product = await this.productModel.findById(productId);
+        return product;
+    }
+
+    async createProduct(createProductDTO: CreateProductDTO): Promise<Product> {
+        const newProduct = new this.productModel(createProductDTO);
+        return await newProduct.save();
+    }
+
+    async deleteProductById(productId: string): Promise<Product>{
+        const deletedProduct = await this.productModel.findByIdAndDelete(productId);
+        return deletedProduct;
+    }
+
+    async updateProduct(productId: string, createProductDTO: CreateProductDTO): Promise<Product>{
+        const updatedProduct = await this.productModel.findByIdAndUpdate(productId, createProductDTO, { new: true });
+        return updatedProduct;
+    }
+}
